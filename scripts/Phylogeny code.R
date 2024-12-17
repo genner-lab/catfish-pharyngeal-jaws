072e02da917c567dbb62f517eb78680ff7e9df7d
CatfishIchthyology <135717697+CatfishIchthyology@users.noreply.github.com>
2024-12-17 16:20:29 +0000

    Add files via upload

--- scripts/Phylogeny code.R
+++ scripts/Phylogeny code.R
@@ -8,11 +8,12 @@ library(treeio)
 library(here)
 library(readr)
 library(dplyr)
+setwd("C:/Users/sl22263/OneDrive - University of Bristol/Documents/Data to be landmarked/phylogeny")
 
 # read ml tree, bootstrap trees, species list
-tr <- ape::read.tree(here::here("assets/bootstrapped.raxml.support"))
-bs <- ape::read.tree(here::here("assets/concatenated.aligned.trimmed.fasta.raxml.bootstraps"))
-keep <- readr::read_csv(here::here("assets/Species.csv"),show_col_types=FALSE) |> dplyr::pull(Name)
+tr <- ape::read.tree(("bootstrapped.raxml.support"))
+bs <- ape::read.tree(("concatenated.aligned.trimmed.fasta.raxml.bootstraps"))
+keep <- readr::read_csv(("Species.csv"),show_col_types=FALSE) |> dplyr::pull(Name)
 
 # tree pruning and rooting function
 tree_prune <- function(tr,tips,root) {
@@ -36,13 +37,8 @@ tr.sub.root$node.label <- boot
 tr.sub.root$node.label[1] <- ""
 
 # write out
-ape::write.tree(tr.sub.root,here::here("assets/bootstrapped.raxml.support.recalc.nwk"))
+ape::write.tree(tr.sub.root,here::here("bootstrapped.raxml.support.recalc.nwk"))
 
-
-
-
-
-setwd("C:/Users/sl22263/OneDrive - University of Bristol/Documents/Data to be landmarked/phylogeny")
 tree <- read.tree("bootstrapped.raxml.support.recalc.nwk")
 ggplot(tree) + geom_tree() + theme_tree() + geom_nodelab(aes(label=node))
 ggtree(tree)
@@ -69,11 +65,39 @@ species <- c("Acanthicus_hystrix", "Ancistrus_brevipinnis", "Ancistrus_ranunculu
              "Rhinelepis_aspera", "Rineloricaria_lanceolata", "Scobinancistrus_aureatus", "Spatuloricaria_cf.evansii_LBP", "Spectracanthicus_immaculatus", "Spectracanthicus_zuanoni", "Sturisoma_robustum", "Sturisomatichthys_tamanae")
 pruned.tree <- keep.tip(rooted_tree, species)
 ggtree(pruned.tree)
+
 #changed node names
 ID <- read.csv("Species.csv", header = T)
 ID1 <-as.data.frame(ID)
 namecorrected <-rename_taxa(pruned.tree, ID1, key = 1, value = 2)
 plot(namecorrected)
+
+traits <- namecorrected %<+% jaw 
+  
+####ancestral states
+jaw <- read.csv("pharyngealjawtypenodiet.csv",row.names=1)
+jaw<-as.matrix(jaw)[,1]
+jaw
+namecorrected$node.label <- NULL
+library(geiger)
+fitDiscrete(namecorrected, jaw,
+            model = "SYM")
+#####aic summary:
+#log-likelihood = -35.511902
+#AIC = 101.023804
+#AICc = 111.023804
+#free parameters = 15
+fitDiscrete(namecorrected, jaw,
+            model = "ER")
+###AIC summary
+#log-likelihood = -43.963604
+#AIC = 89.927209
+#AICc = 89.991725
+#free parameters = 1
+
+fitER<-ace(jaw,namecorrected,model="ER",type="discrete", CI = TRUE)
+fitER
+  
 #final tree
 final <- ggtree(namecorrected) + geom_tiplab(geom = "text", aes(fontface = "italic")) +
   geom_nodelab(aes(label = label, subset = !is.na(as.numeric(label)) & as.numeric(label) > 70
@@ -87,6 +111,21 @@ plot(final)
 #flip to Planiloriaria at top. 
 rotated <- flip(final, 85, 70)
 plot(rotated)
+final <- ggtree(rotated) + geom_tiplab() +
+  geom_point2(aes(colour = "black", label = label, size = 0.8, subset = !is.na(as.numeric(label)) & as.numeric(label) >70)) +
+  xlim(NA, 0.8) 
+plot(final)
+########################################
+final <- ggtree(namecorrected) + xlim(NA, 0.8) 
+plot(final)
+#flip to Planiloriaria at top. 
+rotated <- flip(final, 85, 70)
+rotated1 <- rotated %<+% jaw
+plot(rotated)
+final <- ggtree(rotated) + geom_tiplab() +
+  geom_point2(aes(colour = "black", label = label, size = 0.8, subset = !is.na(as.numeric(label)) & as.numeric(label) >70)) +
+  xlim(NA, 0.8) 
+plot(final)
 
 #mapping traits
 jaw <- read.csv("pharyngealjawtypenodiet.csv")
@@ -97,6 +136,7 @@ final <- rotated %<+% jaw + geom_tippoint(aes(colour = Jaw, label = NULL), posit
   theme(legend.position = "top left") 
 
 plot(final)
+x<-getStates(rotated1,"jaw")
 
 #basic tree 
 ggtree(namecorrected) +
